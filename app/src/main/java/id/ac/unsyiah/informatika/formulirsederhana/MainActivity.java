4e61bbb9a89d5a122b2f6242c3bcbff359548c7f
fueerqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2016-03-05 15:26:31 +0700

    OnClickListener dan Toast

--- app/src/main/java/id/ac/unsyiah/informatika/formulirsederhana/MainActivity.java
+++ app/src/main/java/id/ac/unsyiah/informatika/formulirsederhana/MainActivity.java
@@ -2,9 +2,11 @@ package id.ac.unsyiah.informatika.formulirsederhana;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*buat objeknya*/
-        TextView txtNama, txtNim, txtHp, txtAlamat;
-        EditText edtNama, edtNim, edtHp, edtAlamat;
+        final TextView txtNama, txtNim, txtHp, txtAlamat;
+        final EditText edtNama, edtNim, edtHp, edtAlamat;
         Button btnSimpan;
 
         /*inisialisasi objek nya*/
@@ -30,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
         edtAlamat = (EditText) findViewById(R.id.edit_alamat);
 
         btnSimpan = (Button) findViewById(R.id.button);
+
+        /*
+        kasus 1 :
+        isi semua field,
+        klik tombol,
+        masukkan semua isian ke string,
+        tampilkan toast data isian,
+        edit text jadi kosong
+        */
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = "Nama : " + edtNama.getText() + "\nNim : "
+                        + edtNim.getText() + "\nNo. Hp : " + edtHp.getText()
+                        + "\nAlamat : " + edtAlamat.getText();
+
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+
+                edtNama.setText("");
+                edtNim.setText("");
+                edtHp.setText("");
+                edtAlamat.setText("");
+            }
+        });
     }
 }
